81ef2d871acd17abf6666c831aa8280b69bb69e4
Morgan S <morgan.stewart31@gmail.com>
2024-12-12 01:27:54 -0500

    Inital Commit
    Uploading key add-on files: manifest.json && weightLoss.js
    Also uploading key files as zip

---
+++ weightLoss.js
@@ -0,0 +1,23 @@
+let rightRail = document.querySelector("aside.page__right-rail");
+rightRail.remove();
+
+let topAdContainer = document.querySelector("div.top-ads-container");
+topAdContainer.remove();
+
+let globalNav = document.querySelector("div.global-navigation");
+globalNav.remove();
+
+let globalFooter = document.querySelector("footer.global-footer");
+globalFooter.remove();
+
+let bottomAdContainer = document.querySelector("div[class*='bottom-ads-container']");
+bottomAdContainer.remove();
+
+let midPagePlayer = document.querySelector("div#incontent_player");
+midPagePlayer.remove();
+
+let wikiaBar = document.querySelector("div#WikiaBar");
+wikiaBar.remove();
+
+let adSlots = document.querySelector("div[class*='ad-slot-placeholder']");
+adSlots.remove();
